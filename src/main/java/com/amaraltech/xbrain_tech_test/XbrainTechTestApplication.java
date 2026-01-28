b24910b1c227a212852d77aa5ac125a3eff41781
Gustavo Amaral <gustavo.amaral2310@gmail.com>
2026-01-28 18:17:12 -0300

    create project and configure application.properties

---
+++ src/main/java/com/amaraltech/xbrain_tech_test/XbrainTechTestApplication.java
@@ -0,0 +1,13 @@
+package com.amaraltech.xbrain_tech_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainTechTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amaraltech/xbrain_tech_test/XbrainTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaraltech.xbrain_tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
