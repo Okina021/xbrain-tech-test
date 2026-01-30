5c06217f7854e325e0958fe9ea08c1e77a8b4ec2
Gustavo Amaral <gustavo.amaral2310@gmail.com>
2026-01-30 10:50:46 -0300

    criado entidade Venda e dto de vendedor

---
+++ src/main/java/com/amaraltech/xbrain_tech_test/model/Venda.java
@@ -0,0 +1,34 @@
+package com.amaraltech.xbrain_tech_test.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_vendas")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Venda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate dataVenda;
+    private Double valor;
+    private Long vendedorId;
+    private String vendedorNome;
+
+    public Venda(LocalDate dataVenda, Double valor, Long vendedorId, String vendedorNome) {
+        this.dataVenda = dataVenda;
+        this.valor = valor;
+        this.vendedorId = vendedorId;
+        this.vendedorNome = vendedorNome;
+    }
+}
---
+++ src/main/java/com/amaraltech/xbrain_tech_test/model/VendedorDTO.java
@@ -0,0 +1,9 @@
+package com.amaraltech.xbrain_tech_test.model;
+
+
+public record VendedorDTO(
+        String nome,
+        Double totalVendas,
+        Double mediaDiaria
+) {
+}
